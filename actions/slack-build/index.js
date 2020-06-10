4f7f918d310d9ec2ec44cd18828a511ed96bf648
Daniel Lockyer <hi@daniellockyer.com>
2020-06-10 09:27:19 +0100

    Added basic output
    no issue

--- actions/slack-build/index.js
+++ actions/slack-build/index.js
@@ -1,10 +1,23 @@
 const { IncomingWebhook } = require('@slack/webhook');
+const core = require('@actions/core');
 
 const url = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(url);
 
+const statusInput = core.getInput('status', { required: true });
+
+const githubRepo = process.env.GITHUB_REPOSITORY;
+const githubSha = process.env.GITHUB_SHA;
+const githubRunId = process.env.GITHUB_RUN_ID;
+
+const commitUrl = `https://github.com/${githubRepo}/commit/${githubSha}`;
+const commitActor = process.env.GITHUB_ACTOR;
+const commitBranch = process.env.GITHUB_REF.split('/')[0];
+
+const linkifiedGithubRepo = `[${githubRepo}](https://github.com/${githubRepo})`;
+
 (async () => {
     await webhook.send({
-        text: 'test',
+        text: `Build ${githubRunId}: ${commitUrl} of ${linkifiedGithubRepo}@${commitBranch} by ${commitActor} ${statusInput}`,
     });
 })();
