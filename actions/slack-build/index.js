b45f8fc8d57b0f1a3b8e45138c199de62026967c
Daniel Lockyer <hi@daniellockyer.com>
2021-01-21 11:35:08 +0000

    Added branch to notification text

--- actions/slack-build/index.js
+++ actions/slack-build/index.js
@@ -1,10 +1,10 @@
-const { IncomingWebhook } = require('@slack/webhook');
+const {IncomingWebhook} = require('@slack/webhook');
 const core = require('@actions/core');
 
 const url = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(url);
 
-const statusInput = core.getInput('status', { required: true });
+const statusInput = core.getInput('status', {required: true});
 
 let statusColor = 'good';
 
@@ -17,16 +17,19 @@ if (statusInput === 'cancelled') {
 const githubRepo = process.env.GITHUB_REPOSITORY;
 const githubSha = process.env.GITHUB_SHA;
 const githubActor = process.env.GITHUB_ACTOR;
+const githubRef = process.env.GITHUB_REF;
 
 const commitUrl = `https://github.com/${githubRepo}/commit/${githubSha}`;
 const linkifiedGithubRepo = `<https://github.com/${githubRepo}|${githubRepo}>`;
 const linkifiedCommitUrl = `<${commitUrl}|${githubSha.substring(0, 10)}>`;
+const branchParts = githubRef.split('/');
+const branch = branchParts[branchParts.length - 1];
 
 (async () => {
     await webhook.send({
         attachments: [{
             color: statusColor,
-            text: `Build ${statusInput} at ${linkifiedCommitUrl} of ${linkifiedGithubRepo} by ${githubActor}`,
+            text: `Build ${statusInput} at ${linkifiedCommitUrl} on ${branch} of ${linkifiedGithubRepo} by ${githubActor}`,
         }]
     });
 })();
