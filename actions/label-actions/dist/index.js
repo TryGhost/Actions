282cc61bb71fa4c5ec237a8a40003f2f73b3e29d
Daniel Lockyer <hi@daniellockyer.com>
2021-11-08 21:27:14 +0000

    Added `needs triage` label upon issue creation
    refs https://github.com/TryGhost/Toolbox/issues/81
    
    - this should add the `needs triage` label when an issue is opened
    - also fixes a logging issue - `core.warn` is not a thing

--- actions/label-actions/dist/index.js
+++ actions/label-actions/dist/index.js
@@ -8453,17 +8453,28 @@ async function main() {
     }
 
     const client = github.getOctokit(githubToken);
-    const payload = github.context.payload;
+    const {payload, repo} = github.context;
 
     // We only want to do something when a human labels an issue
     if (payload.sender.type === 'Bot') {
-        core.warn('Ignoring event, detected a bot');
+        core.info('Ignoring event, detected a bot');
         return;
     }
 
     // We can only handle issue-related events right now
     if (!payload.issue) {
-        core.warn('Ignoring event, detected a non-issue event');
+        core.info('Ignoring event, detected a non-issue event');
+        return;
+    }
+
+    const issue = payload.issue;
+
+    if (payload.action === 'opened') {
+        await client.rest.issues.addLabels({
+            ...repo,
+            issue_number: issue.number,
+            labels: ['needs triage']
+        });
         return;
     }
 
