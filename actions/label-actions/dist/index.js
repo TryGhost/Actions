8448e3661c6d66c7fff8d8b6a8df9d414cf62a04
Daniel Lockyer <hi@daniellockyer.com>
2021-11-11 18:28:07 +0000

    Updated comment contents
    refs https://github.com/TryGhost/Toolbox/issues/81
    
    - fills in a lot of the TODOs that were previously present

--- actions/label-actions/dist/index.js
+++ actions/label-actions/dist/index.js
@@ -8277,25 +8277,33 @@ function wrappy (fn, cb) {
 /***/ ((module) => {
 
 module.exports = {
-    SUPPORT_REQUEST: `Hey @{issue-author} :wave: We ask that you please do not use GitHub for help or support 😄. We use GitHub solely for bug-tracking and on-going feature development.
+    SUPPORT_REQUEST: `Hey @{issue-author} 👋 We ask that you please do not use GitHub for help or support 😄. We use GitHub solely for bug-tracking and community-driven development.
 
-Many questions can be answered by reviewing our [documentation](https://ghost.org/docs/). If you can't find an answer then our [forum](https://forum.ghost.org) is a great place to get community support, plus it helps create a central location for searching problems/solutions.
+Many questions can be answered by reviewing our [documentation](https://ghost.org/docs/). If you can't find an answer then our [forum](https://forum.ghost.org/) is a great place to get community support, plus it helps create a central location for searching problems/solutions.
 
-FYI: Many projects have their own support guidelines and GitHub will highlight them for you as it did here, or the project owners will use issue templates to point you in the right direction. Reading the guidelines or issue templates before opening issues can save you and project maintainers valuable time.`,
+FYI: Many projects use issue templates to point you in the right direction. Reading the guidelines or issue templates before opening issues can save you and project maintainers valuable time.`,
 
-    FEATURE_REQUEST: `Hey @{issue-author} :wave: Friendly reminder: we don't track feature requests on GitHub.
+    FEATURE_REQUEST: `Hey @{issue-author} 👋
+
+Friendly reminder: we don't track feature requests on GitHub.
 
 Please look for similar ideas to vote for on the [forum](https://forum.ghost.org/c/Ideas/l/votes) and if you can't anything similar then post your own idea.
 
-FYI: Many projects have their own support guidelines and GitHub will highlight them for you as it did here, or the project owners will use issue templates to point you in the right direction. Reading the guidelines or issue templates before opening issues can save you and project maintainers valuable time.`,
+FYI: Many projects use issue templates to point you in the right direction. Reading the guidelines or issue templates before opening issues can save you and project maintainers valuable time.`,
 
-    NEEDS_INFO: `TODO`,
+    NEEDS_INFO: `Note from our bot: The \`needs info\` label has been added to this issue. Updating your original issue with more details is great, but won't notify us, so please make sure you leave a comment so that we can see when you've updated us.`,
 
-    NEEDS_TEMPLATE: `TODO`,
+    NEEDS_TEMPLATE: `Hey @{issue-author} 👋
 
-    NO_UPDATE: `TODO: THERE HAS BEEN NO UPDATE`,
+Looks like you've stumbled upon a bug, but we don't have enough information here to help.
 
-    PING_ASSIGNEE: `TODO: This issue is currently awaiting triage from @{issue-assignee}. We're in a busy time right now, but we'll update this issue ASAP.`
+Please reopen this issue using our [bug report template](https://github.com/TryGhost/Ghost/issues/new?assignees=&labels=&template=bug_report.yml), including as much info as possible and we will do our best to help track it down.`,
+
+    NO_UPDATE: `Hey @{issue-author} 👋
+
+Our team needed some more info to get to the bottom of this, however we've not heard back from you. We're going to close this for now, but let us know if you manage to dig up some more info and we'll reopen.`,
+
+    PING_ASSIGNEE: `This issue is currently awaiting triage from @{issue-assignee}. We're having a busy time right now, but we'll update this issue ASAP. If you have any more information to help us triage faster please leave us some comments. Thank you for understanding 🙂`
 };
 
 
