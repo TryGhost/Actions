d92c14ab1a78dcad9d0bd4228210cd87e841101c
Daniel Lockyer <hi@daniellockyer.com>
2023-01-26 16:01:21 +0100

    Added link to run in Slack notification
    - also updated minor copy

--- actions/slack-build/index.js
+++ actions/slack-build/index.js
@@ -15,9 +15,10 @@ if (statusInput === 'cancelled') {
 }
 
 const githubRepo = process.env.GITHUB_REPOSITORY;
-const githubSha = process.env.GITHUB_SHA;
+const githubSha = process.env.GITHUB_SHA || 'unknown';
 const githubActor = process.env.GITHUB_ACTOR;
-const githubRef = process.env.GITHUB_REF;
+const githubRef = process.env.GITHUB_REF || 'unknown';
+const githubRunId = process.env.GITHUB_RUN_ID;
 
 const commitUrl = `https://github.com/${githubRepo}/commit/${githubSha}`;
 const linkifiedGithubRepo = `<https://github.com/${githubRepo}|${githubRepo}>`;
@@ -25,11 +26,13 @@ const linkifiedCommitUrl = `<${commitUrl}|${githubSha.substring(0, 10)}>`;
 const branchParts = githubRef.split('/');
 const branch = branchParts[branchParts.length - 1];
 
+const openLink = `<https://github.com/${githubRepo}/runs/${githubRunId}|view>`;
+
 (async () => {
     await webhook.send({
         attachments: [{
             color: statusColor,
-            text: `Build ${statusInput} at ${linkifiedCommitUrl} on ${branch} of ${linkifiedGithubRepo} by ${githubActor}`,
+            text: `Test ${statusInput} at ${linkifiedCommitUrl} on \`${branch}\` of ${linkifiedGithubRepo} by ${githubActor} - ${openLink}`,
         }]
     });
 })();
