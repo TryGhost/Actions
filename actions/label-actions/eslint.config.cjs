44ce6d5bfa6e82f87cb6ac706e78095c7634e74b
John O'Nolan <john@onolan.org>
2026-03-17 23:03:35 +0400

    Restore label-actions test lint rules (#83)
    Keep the ESLint 10 branch from dropping the test lint contract in label-actions.
    
    Retain the ghost test rules that still work under ESLint 10 and filter only the mocha rules that are incompatible with the new ESLint API.

--- actions/label-actions/eslint.config.cjs
+++ actions/label-actions/eslint.config.cjs
@@ -4,12 +4,20 @@ const globals = require('globals');
 
 const baseConfig = require(require.resolve('eslint-plugin-ghost/lib/config/base.js'));
 const nodeConfig = ghost.configs.node;
+const testConfig = ghost.configs.test;
 const {
     ['ghost/node/no-restricted-require']: _noRestrictedRequire,
     ['ghost/filenames/match-exported-class']: _matchExportedClass,
     ['ghost/filenames/match-regex']: _matchRegex,
     ...nodeRules
 } = nodeConfig.rules;
+const {
+    ['ghost/mocha/no-global-tests']: _noGlobalTests,
+    ['ghost/mocha/max-top-level-suites']: _maxTopLevelSuites,
+    ['ghost/mocha/no-mocha-arrows']: _noMochaArrows,
+    ['ghost/mocha/no-async-describe']: _noAsyncDescribe,
+    ...testRules
+} = testConfig.rules;
 
 module.exports = [
     {
@@ -49,7 +57,8 @@ module.exports = [
                 should: 'readonly',
                 sinon: 'readonly'
             }
-        }
+        },
+        rules: testRules
     },
     {
         files: ['src/helpers.js'],
