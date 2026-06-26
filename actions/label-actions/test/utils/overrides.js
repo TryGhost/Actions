60396fc38e023e9b758dc04d2bebb1af60410df7
Aileen Booker <AileenCGN@gmail.com>
2026-06-26 15:34:07 +0400

    Fixed explicit `sinon` import in label-action tests
    The helper tests were still relying on `test/utils` to attach `sinon` to `global`, which made the file less self-contained after the oxlint migration. Importing `sinon` directly matches the other test files and lets the shared test setup keep only the assertion globals it still owns.

--- actions/label-actions/test/utils/overrides.js
+++ actions/label-actions/test/utils/overrides.js
@@ -3,6 +3,3 @@
 // Taken from the should wiki, this is how to make should global.
 global.should = require('should').noConflict();
 should.extend();
-
-// Sinon is a simple case.
-global.sinon = require('sinon');
